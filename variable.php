db369350abc2fa34de8998a3e4bc2d4570c72ad8
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 11:08:41 +0600

    first commit

---
+++ variable.php
@@ -0,0 +1,23 @@
+<?php
+$price = "Price: 200 BDT";
+$Price = " Price: 300 BDT";
+
+echo $price;
+echo $Price;
+
+$mark = 70;
+$mark = 60;
+
+echo $mark;
+
+$mark1 = 70;
+$mark2 = $mark1;
+
+echo $mark2;
+
+$a = $b;
+$b = 20;
+
+echo $a;
+
+?>
